b30f4fdb20c52759bd00847effad644bf7fe4bc7
Kris <krisgodom@gmail.com>
2020-12-16 23:16:54 -0400

    added Test class

---
+++ Test/com.go2it.edu/Task5_4/BranchManagerTest.java
@@ -0,0 +1,12 @@
+package com.go2it.edu.Task5_4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BranchManagerTest {
+    @Test
+    public void testExample (){
+        assertEquals(3, 2+1);
+    }
+}
