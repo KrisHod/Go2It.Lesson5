bd7dd393f0926f8bd127a82d7b2ad5739a1c899c
Kris <krisgodom@gmail.com>
2020-12-17 22:30:04 -0400

    refactoring

--- src/AccountManagingSysterm/AccountMain.java
+++ src/AccountManagingSysterm/AccountMain.java
@@ -4,6 +4,8 @@ public class AccountMain {
     public static void main(String[] args) {
         AccountManager.addAccount(2345, "John", 3000, 100);
         AccountManager.addAccount(2346, "Karolina", 8000, 100);
+        SavingAccount account = (SavingAccount)AccountManager.getAccountList()[0];
+        account.isPayingExtraCharge();
 
         System.out.println(AccountManager.printAccountDetails(2345));
         AccountManager.listAllAccounts();
