b8c6187669cd40e26c2464867ef31cbd7bafb10e
Kris <krisgodom@gmail.com>
2020-12-25 22:02:20 -0400

    added gitignore

--- src/AccountManagingSysterm/AccountMain.java
+++ src/AccountManagingSysterm/AccountMain.java
@@ -4,10 +4,12 @@ public class AccountMain {
     public static void main(String[] args) {
         AccountManager.addAccount(2345, "John", 3000, 100);
         AccountManager.addAccount(2346, "Karolina", 8000, 100);
-        SavingAccount account = (SavingAccount)AccountManager.getAccountList()[0];
+        SavingAccount account = (SavingAccount) AccountManager.getAccountList()[0];
         account.isPayingExtraCharge();
 
         System.out.println(AccountManager.printAccountDetails(2345));
         AccountManager.listAllAccounts();
     }
+
+
 }
